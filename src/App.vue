a89264c593988540ff4b15e19f4b317c4ef9db4f
dongergan@163.com <dongergan@163.com>
2019-08-30 11:36:55 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+  <Header></header>
+  <div class="main">
+    <router-view name="slidebar"></router-view>
+    <router-view name="main"></router-view>
+    
+  </div>
+  </div>
+</template>
+
+
+<script>
+import Header from './components/Header'
+import PostList from './components/PostList'
+import Arcicle from './components/Article'
+import UserInfo from './components/UserInfo'
+
+export default {
+  name: 'App',
+  components:{
+    Header,PostList,Arcicle,
+  }
+}
+</script>
+<style >
+ .main {
+   width:80%;
+   margin:0 auto;
+ }
+</style>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="Header">
+        <router-link :to="{
+            name:'root'
+        }">
+            <img src="../assets/cnodejs_light.svg" alt="">
+        </router-link>
+        
+        <ul>
+            <li><a href="">首页</a></li>
+            <li><a href="">新手入门</a></li>
+            <li><a href="">API</a></li>
+            <li><a href="">关于</a></li>
+            <li><a href="">注册</a></li>
+            <li><a href="">登陆</a></li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+<style scoped>
+.Header {
+    background-color: #5a5555;
+    height:50px;
+}
+a {
+    text-decoration: none;
+    color: #ccc;
+    padding: 5px;
+    margin:5px;
+    text-shadow: none;
+}
+li {
+    list-style:none;
+    display:inline-block;
+}
+ul {
+    float:right;
+}
+img {
+    max-width: 120px;
+    margin-left: 50px;
+    margin-top: 10px;
+}
+</style>
